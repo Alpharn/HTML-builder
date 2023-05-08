5d9f56a0174ec9c1448cb01c5bf7ba722196b79f
Alpharn <82173835+Alpharn@users.noreply.github.com>
2023-05-08 21:20:51 +0300

    feat: add task2 solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const writeStream = fs.createWriteStream(path.join(__dirname, 'output.txt'), { flags: 'a' });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+console.log('Type your text:');
+rl.on('line', (input) => {
+  if (input.trim().toLowerCase() === 'exit') {
+    console.log('Bye!');
+    rl.close();
+  } else {
+    writeStream.write(`${input}\n`);
+    console.log('Text added to file');
+  }
+});
+process.on('SIGINT', () => {
+    console.log('\nBye!');
+    rl.close();
+  });
